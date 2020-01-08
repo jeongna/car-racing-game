07d5299c486d6d0a5cdae2ba5734c427fb52d5e4
jny <jny@DESKTOP-DGUS06M>
2020-01-08 18:17:32 +0900

    Update Main method

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,8 +3,8 @@ package domain;
 public class Main {
 	
 	public static void main(String[] args) {
-		//Racing game = new Racing();
-		//game.start();
+		Racing game = new Racing();
+		game.start();
 	}
 
 }
